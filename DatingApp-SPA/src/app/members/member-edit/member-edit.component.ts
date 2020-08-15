3969b1513c53ee1079e7d524d676e5e335d071be
sharaf wada <a.awad1983@gmail.com>
2020-08-15 13:36:23 +0200

    member-edit

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,12 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,private alertifyService:AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data.user;  // data['user']
     });
+  }
+  updateUser(){
+    console.log(this.user);  
+    this.alertifyService.success("Profile uptaded successfully");
   }
 
 }
